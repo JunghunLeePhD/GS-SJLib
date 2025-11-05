a5e5008e867620934434c4f8e3c2f14e8e3d832b
junghun <yijeonghunxd@gmail.com>
2025-11-05 03:30:55 +0000

    Update (`Code.js`): change the order of functions
    It is easier to run the script for setting trigger on the web editor.

--- src/Code.js
+++ src/Code.js
@@ -1,3 +1,30 @@
+/**
+ * Deletes all existing triggers for the main function and
+ * creates a new 30-minute time-based trigger.
+ * Run this using 'clasp run setupTrigger'.
+ */
+function setupTrigger() {
+  var functionToRun = "saveHtmlToDriveInFolder";
+
+  // 1. Delete all existing triggers for this function to prevent duplicates
+  var allTriggers = ScriptApp.getProjectTriggers();
+  for (var i = 0; i < allTriggers.length; i++) {
+    if (allTriggers[i].getHandlerFunction() === functionToRun) {
+      ScriptApp.deleteTrigger(allTriggers[i]);
+      Logger.log("Deleted existing trigger: " + allTriggers[i].getUniqueId());
+    }
+  }
+
+  // 2. Create the new 30-minute trigger
+  ScriptApp.newTrigger(functionToRun)
+    .timeBased()
+    .everyMinutes(15) // <-- You can change this to 15 if you want
+    .create();
+
+  Logger.log("Successfully created new trigger for " + functionToRun);
+  return "Trigger created successfully.";
+}
+
 function saveHtmlToDriveInFolder() {
   // --- Time Check Logic ---
   var now = new Date();
@@ -103,31 +130,4 @@ function saveHtmlToDriveInFolder() {
         ":00). Skipping execution."
     );
   }
-}
-
-/**
- * Deletes all existing triggers for the main function and
- * creates a new 30-minute time-based trigger.
- * Run this using 'clasp run setupTrigger'.
- */
-function setupTrigger() {
-  var functionToRun = "saveHtmlToDriveInFolder";
-
-  // 1. Delete all existing triggers for this function to prevent duplicates
-  var allTriggers = ScriptApp.getProjectTriggers();
-  for (var i = 0; i < allTriggers.length; i++) {
-    if (allTriggers[i].getHandlerFunction() === functionToRun) {
-      ScriptApp.deleteTrigger(allTriggers[i]);
-      Logger.log("Deleted existing trigger: " + allTriggers[i].getUniqueId());
-    }
-  }
-
-  // 2. Create the new 30-minute trigger
-  ScriptApp.newTrigger(functionToRun)
-    .timeBased()
-    .everyMinutes(15) // <-- You can change this to 15 if you want
-    .create();
-
-  Logger.log("Successfully created new trigger for " + functionToRun);
-  return "Trigger created successfully.";
 }
